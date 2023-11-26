0f75cc1e7d48d10803b1c65ea9a74e2016f248d5
abdelrahman <aboabdo8600@gmail.com>
2023-11-27 01:52:41 +0200

    T-shirt

--- src/pages/Reviews/Reviews.js
+++ src/pages/Reviews/Reviews.js
@@ -8,15 +8,15 @@ const Reviews = () => {
     <div className='container Reviews  mt-3 mb-5 p-3' >
      <div className='row '>
      <h1>Add your Rating</h1>
-     {/* <div className="container mt-3 "> */}
-     <form className='shadow-lg p-2 w-100 rounded-5 bg-dark text-light'>
+     <div className=" mt-3 ">
+     <form className='shadow-lg p-3  rounded-5 bg-dark text-light'>
      <textarea className='form-control mt-2 rounded-3 'placeholder='Add Your Feedback' ></textarea>
 
    <div className='star' >  <ReactStars  count={5} size={24} color2={"#ffd700"} /></div>
 
      <input type='submit' className='btn btn-outline-primary my-3' value={"Send"} />
      </form>
-     {/* </div> */}
+     </div>
     </div></div>
   )
 }
